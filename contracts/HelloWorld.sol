b3343a31a7c14d1b4becb0f1b306f5fb3be076fc
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-04-25 18:58:28 +0100

    Initial commit & Smart Contract Written

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.9;
+
+contract HelloWorld {
+    string public yourName;
+    constructor () public {
+        yourName = "Unknown";
+    }
+
+    function setName (string memory nm) public {
+        yourName = nm;
+    }
+}
